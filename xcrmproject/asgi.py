db3ede883adfa566584547152146c7ce24a09b51
richardxlc <86317810@163.com>
2021-06-28 18:23:18 +0800

    first test

---
+++ xcrmproject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for xcrmproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xcrmproject.settings')
+
+application = get_asgi_application()
