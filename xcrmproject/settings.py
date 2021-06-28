12365e1b0202d4ef8f9e8217cd62680b933bfeba
richardxlc <86317810@163.com>
2021-06-28 21:35:15 +0800

    20210628 updates

--- xcrmproject/settings.py
+++ xcrmproject/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #第三方的packages
+
+    #自建的app
+    'leads'
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'leads.user'
