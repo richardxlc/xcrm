4a0a045d43b327477a97d0f04b74f45ba8ebbb5e
richardxlc <86317810@163.com>
2021-07-01 11:55:11 +0800

    03 20210701

--- leads/models.py
+++ leads/models.py
@@ -11,8 +11,16 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
 class Agent(models.Model):
     user = models.OneToOneField(User,related_name='agent',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+
+
 
 
 
