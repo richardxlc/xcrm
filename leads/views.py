42743c83cd04d096012ff650063304dc18684adf
richardxlc <86317810@163.com>
2021-07-05 09:10:45 +0800

    20

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Lead
 from django.http import HttpResponse
 
-def home_page(request):
-    return HttpResponse("Hello world")
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+       "leads":leads
+    }
+    return render(request,"leads/lead_list.html",context)
+
+def lead_detail(request,pk):
+   lead = Lead.objects.get(id=pk)
+   context={
+      'lead':lead
+   }
+   return render(request,"leads/lead_detail.html",context)
+
